6610b466c28a877d8be8143b6d29cec8c52faca5
oonakm <oona.maattala@hotmail.com>
2022-02-21 23:42:03 +0200

    k

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+
+
+const URL = 'http://api.exchangeratesapi.io/vl/latest?access_key=';
+const API_KEY = '93bd388ed0120d0f91bd81787384f3e9';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [eur,setEur] = useState(0);
+    const [gbp,setGbp] = useState(0);
+    const [rate,setRate] = useState(0);
+
+return (
+    <div id="container">
+        <form onSubmit={convert}>
+            <div>
+                <label>Eur</label>&nbsp;
+                <input type="number" step="0.01"
+                value={eur} onChange={e => setEur(e.target.value)}></input>
+                <output>{rate}</output>
+            </div>
+            <div>
+                <label>Gbp</label>
+                <output>{gbp.toFixed(2)} €</output>
+                </div>
+                <div>
+                    <button>Calculate</button>
+                </div>
+        </form>
     </div>
-  );
+    );
+}
+
+async function convert(e) {
+    e.preventDefault();
+    try {
+        const address = URL + API_KEY;
+        const response = await fetch(address);
+
+        if (response.ok) {
+            const json = await response.json();
+            console.log(json.rates.GBP)
+            setRate(json.rates.GBP);
+
+            setGbp(eur * json.rates.GBP);
+        } else {
+            alert('Error retrieving exchange rate.');
+            console.log(response);
+        }
+    } catch (err) {
+        alert(err);
+    }
 }
 
 export default App;
